2c67604875a85367be77485401feaf259e4a7608
asad2002web <asad2002web@gmail.com>
2023-07-24 10:21:41 -0700

    user route create

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 //================= MongoDB Connected =================
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1zypahx.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -19,31 +19,46 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-// ---------------------------
+    // ---------------------------
+
+    const userCollection = client.db("collegeConnect").collection("users");
+
+    app.post("/users", async (req, res) => {
+      const UserInfo = req.body;
+      const result = await userCollection.insertOne(UserInfo);
+      res.send(result);
+    });
+
+    app.get("/users", async (req, res) => {
+      try {
+        const result = await userCollection.find().toArray();
+        res.send(result);
+      } catch (error) {
+        console.log(error.message)
+      }
+    });
 
 
 
-
-
-
-// ---------------------------
+    // ---------------------------
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
 
 // Define a route
 app.get("/", (req, res) => {
